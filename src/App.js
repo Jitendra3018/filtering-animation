b687fc313583a313d0a01bfa1868885aaff3f9a9
Jitendra Garg <jeetugarg69@gmail.com>
2022-02-09 01:09:51 +0530

    Filtrating of Movies Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Movie from "./Movie";
+import "./App.css";
+import Filter from "./Filter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [popular, setPopular] = useState([]);
+	const [filtered, setFiltered] = useState([]);
+	const [activeGenre, setActiveGenre] = useState(0);
+
+	useEffect(() => {
+		fetchPopular();
+	}, []);
+
+	const fetchPopular = async () => {
+		const data = await fetch(
+			"https://api.themoviedb.org/3/movie/popular?api_key=d7c3c1b7af42d6043e0729870317e71a&language=en-US&page=1"
+		);
+		const movies = await data.json();
+		console.log(movies.results);
+
+		setPopular(movies.results);
+		setFiltered(movies.results);
+	};
+
+	return (
+		<div className="App">
+			<Filter
+				popular={popular}
+				setFiltered={setFiltered}
+				activeGenre={activeGenre}
+				setActiveGenre={setActiveGenre}
+			/>
+			<div className="popularMovies">
+				{filtered.map((movie) => (
+					<Movie key={movie.id} movie={movie} />
+				))}
+			</div>
+		</div>
+	);
 }
-
-export default App;
---
+++ src/Filter.js
@@ -0,0 +1,80 @@
+import { useEffect } from "react";
+
+export default function Filter({
+	setActiveGenre,
+	activeGenre,
+	setFiltered,
+	popular,
+}) {
+	useEffect(() => {
+		// console.log("hello");
+		if (activeGenre === 0) {
+			setFiltered(popular);
+			return;
+		}
+
+		const filtered = popular.filter((movie) =>
+			movie.genre_ids.includes(activeGenre)
+		);
+		setFiltered(filtered);
+	}, [activeGenre]);
+
+	return (
+		<div className="filterContainer">
+			<button
+				className={activeGenre === 0 ? "active" : ""}
+				onClick={() => setActiveGenre(0)}
+			>
+				All
+			</button>
+			<button
+				className={activeGenre === 28 ? "active" : ""}
+				onClick={() => setActiveGenre(28)}
+			>
+				Action
+			</button>
+			<button
+				className={activeGenre === 12 ? "active" : ""}
+				onClick={() => setActiveGenre(12)}
+			>
+				Adventure
+			</button>
+			<button
+				className={activeGenre === 16 ? "active" : ""}
+				onClick={() => setActiveGenre(16)}
+			>
+				Animation
+			</button>
+			<button
+				className={activeGenre === 35 ? "active" : ""}
+				onClick={() => setActiveGenre(35)}
+			>
+				Comedy
+			</button>
+			<button
+				className={activeGenre === 10402 ? "active" : ""}
+				onClick={() => setActiveGenre(10402)}
+			>
+				Music
+			</button>
+			<button
+				className={activeGenre === 18 ? "active" : ""}
+				onClick={() => setActiveGenre(18)}
+			>
+				Drama
+			</button>
+			<button
+				className={activeGenre === 10749 ? "active" : ""}
+				onClick={() => setActiveGenre(10749)}
+			>
+				Romance
+			</button>
+			<button
+				className={activeGenre === 878 ? "active" : ""}
+				onClick={() => setActiveGenre(878)}
+			>
+				Science Fiction
+			</button>
+		</div>
+	);
+}
